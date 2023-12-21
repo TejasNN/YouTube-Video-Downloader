5c526c51c3c4f43d5466562e383232d10a03c450
TejasNN <tejasnagvekar90@gmail.com>
2023-12-21 20:38:20 +0530

    - Added option menu for appearance change

--- YouTubeDownLoader.py
+++ YouTubeDownLoader.py
@@ -1,4 +1,5 @@
 import tkinter
+from tkinter import *
 import customtkinter
 from pytube import YouTube
 
@@ -16,7 +17,7 @@ def start_Download():
     except:
         finishLabel.configure(text="Download Error", text_color= "red")
 
-
+# Function to update the progress percentage and progress bar
 def on_progress(stream, chunk, bytes_remaining):
     total_size = stream.filesize
     bytes_downloaded = total_size - bytes_remaining
@@ -27,6 +28,13 @@ def on_progress(stream, chunk, bytes_remaining):
 
     # Updating progress bar
     progressBar.set(float(percentage_of_completion) / 100)
+
+# Function to add option menu to change appearance mode
+def optionmenu_callback(choice):
+    if choice == "Light":
+        customtkinter.set_appearance_mode("light")
+    elif choice == "Dark":
+        customtkinter.set_appearance_mode("dark")
 
 # System setting
 customtkinter.set_appearance_mode("system")
@@ -39,7 +47,7 @@ app.title("YouTube Downloader")
 
 # Adding UI elements
 title = customtkinter.CTkLabel(app, text= "Insert a youtube link", font= ("Arial",14))
-title.pack(padx= 10, pady= 10)
+title.pack(padx= 10, pady= 20)
 
 # Link input
 urlVar = tkinter.StringVar()
@@ -61,6 +69,11 @@ progressBar.pack(padx= 10, pady= 10)
 # Download button
 download = customtkinter.CTkButton(app, text= "Download", command=start_Download)
 download.pack(padx=10, pady= 10)
+
+# Adding appearance customization
+options = ["Light", "Dark"]
+optionMenu = customtkinter.CTkOptionMenu(app, values=options, height= 30, width= 70, command= optionmenu_callback)
+optionMenu.place(relx = 1, rely= 0, x = -4, y = 4, anchor= NE)
 
 # Run the app
 app.mainloop()
